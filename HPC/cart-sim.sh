8463b7124d86c2dd655a832637c3192d29f3bf7c
jrengle <jrengle@clogin01.cl.emory.edu>
2023-12-31 08:00:35 -0500

    update naming convention for combine scripts

--- HPC/cart-sim.sh
+++ HPC/cart-sim.sh
@@ -7,4 +7,4 @@ array_job3=$(sbatch --parsable HPC/cart-sim.slurm 3)
 array_job4=$(sbatch --parsable HPC/cart-sim.slurm 4)
 array_job5=$(sbatch --parsable HPC/cart-sim.slurm 5)
 
-sbatch --depend=afterany:$array_job5:$array_job4:$array_job3:$array_job2:$array_job1:$array_job0 HPC/cart-combine.slurm
+sbatch --depend=afterany:$array_job5:$array_job4:$array_job3:$array_job2:$array_job1:$array_job0 HPC/cart-sim-combine.slurm
--- HPC/friedman-sim.sh
+++ HPC/friedman-sim.sh
@@ -3,4 +3,4 @@
 array_job0=$(sbatch --parsable HPC/friedman-sim.slurm 0)
 array_job1=$(sbatch --parsable HPC/friedman-sim.slurm 1)
 
-sbatch --depend=afterany:$array_job1:$array_job0 HPC/friedman-combine.slurm
+sbatch --depend=afterany:$array_job1:$array_job0 HPC/friedman-sim-combine.slurm
